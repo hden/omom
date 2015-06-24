2170602561c226d2dc9b2353f3ec5a93d07c00a4
Hao-kang Den <haokang.den@gmail.com>
2015-06-24 15:59:05 +0800

    fix re-render
    component should re-render if child changed

--- src/component.js
+++ src/component.js
@@ -1,7 +1,7 @@
 export default (render, eq = Object.is) => {
   let currentProps, currentChildren, cache
   return (nextProps, nextChildren) => {
-    if (!cache || !eq(currentProps, nextProps || !eq(currentChildren, nextChildren))) {
+    if ((!cache || !eq(currentProps, nextProps) || !eq(currentChildren, nextChildren))) {
       currentProps = nextProps
       currentChildren = nextChildren
       cache = render(currentProps, currentChildren)
