57d0bacd32f760e5683a0d0824ef45d32c1263ad
Hao-kang Den <haokang.den@gmail.com>
2015-06-25 10:54:50 +0800

    fix export

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import h         from './src/h'
-import h         from './src/cx'
+import cx        from './src/cx'
 import loop      from './src/loop'
 import action    from './src/action'
 import component from './src/component'
